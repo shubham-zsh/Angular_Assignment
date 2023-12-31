20fd58d706b6c5d1d9ae176aa31500be8f0884f0
Borse-Shubham <shubhamborse0911@gmail.com>
2023-12-31 17:14:05 +0530

    Even Handling

---
+++ 7/src_3/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  
+  public str : string = "Marvellous Infosystems";
+
+  public DisplayEvent()
+  {
+    this.str = "Educating Better Tommorrow";
+  }
+}
